1702f90616e5fbccdccb4b2e2b8b33e72b93bd62
y-ohgi <ohgi.211@gmail.com>
2019-04-21 21:16:51 +0900

    Add handson template

---
+++ handson/handson/main.tf
@@ -0,0 +1,561 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# VPC
+# https://www.terraform.io/docs/providers/aws/r/vpc.html
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+# Subnet
+# https://www.terraform.io/docs/providers/aws/r/subnet.html
+resource "aws_subnet" "public_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.2.0/24"
+
+  tags = {
+    Name = "handson-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.3.0/24"
+
+  tags = {
+    Name = "handson-public-1d"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.30.0/24"
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+# Internet Gateway
+# https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+# Elasti IP
+# https://www.terraform.io/docs/providers/aws/r/eip.html
+resource "aws_eip" "nat_1a" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1a"
+  }
+}
+
+# NAT Gateway
+# https://www.terraform.io/docs/providers/aws/r/nat_gateway.html
+resource "aws_nat_gateway" "nat_1a" {
+  subnet_id     = "${aws_subnet.public_1a.id}" # NAT Gatewayを配置するSubnetを指定
+  allocation_id = "${aws_eip.nat_1a.id}"       # 紐付けるElasti IP
+
+  tags = {
+    Name = "handson-1a"
+  }
+}
+
+resource "aws_eip" "nat_1c" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1c"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1c" {
+  subnet_id     = "${aws_subnet.public_1c.id}"
+  allocation_id = "${aws_eip.nat_1c.id}"
+
+  tags = {
+    Name = "handson-1c"
+  }
+}
+
+resource "aws_eip" "nat_1d" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1d"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1d" {
+  subnet_id     = "${aws_subnet.public_1d.id}"
+  allocation_id = "${aws_eip.nat_1d.id}"
+
+  tags = {
+    Name = "handson-1d"
+  }
+}
+
+# Route Table
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-public"
+  }
+}
+
+# Route
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.public.id}"
+  gateway_id             = "${aws_internet_gateway.main.id}"
+}
+
+# Association
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = "${aws_subnet.public_1a.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = "${aws_subnet.public_1c.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public_1d" {
+  subnet_id      = "${aws_subnet.public_1d.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+# Route Table (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_route_table" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+resource "aws_route_table" "private_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+# Route (Private)
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "private_1a" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1a.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1a.id}"
+}
+
+resource "aws_route" "private_1c" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1c.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1c.id}"
+}
+
+resource "aws_route" "private_1d" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1d.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1d.id}"
+}
+
+# Association (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = "${aws_subnet.private_1a.id}"
+  route_table_id = "${aws_route_table.private_1a.id}"
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = "${aws_subnet.private_1c.id}"
+  route_table_id = "${aws_route_table.private_1c.id}"
+}
+
+resource "aws_route_table_association" "private_1d" {
+  subnet_id      = "${aws_subnet.private_1d.id}"
+  route_table_id = "${aws_route_table.private_1d.id}"
+}
+
+# SecurityGroup
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group" "alb" {
+  name        = "handson-alb"
+  description = "handson alb"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-alb"
+  }
+}
+
+# SecurityGroup Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = "${aws_security_group.alb.id}"
+
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALB
+# https://www.terraform.io/docs/providers/aws/d/lb.html
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name               = "handson"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
+
+# Listener
+# https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+resource "aws_lb_listener" "main" {
+  # HTTPでのアクセスを受け付ける
+  port     = "80"
+  protocol = "HTTP"
+
+  # ALBのarnを指定します。
+  #XXX: arnはAmazon Resource Names の略で、その名の通りリソースを特定するための一意な名前(id)です。
+  load_balancer_arn = "${aws_lb.main.arn}"
+
+  # "ok" という固定レスポンスを設定する
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+      message_body = "ok"
+    }
+  }
+}
+
+# Task Definition
+# https://www.terraform.io/docs/providers/aws/r/ecs_task_definition.html
+resource "aws_ecs_task_definition" "main" {
+  family = "handson"
+
+  # Fargateの設定
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  # 起動するコンテナの定義
+  container_definitions = <<EOL
+[
+  {
+    "name": "nginx",
+    "image": "nginx:1.14",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOL
+}
+
+# ECS Cluster
+# https://www.terraform.io/docs/providers/aws/r/ecs_cluster.html
+resource "aws_ecs_cluster" "main" {
+  name = "handson"
+}
+
+# ELB Target Group
+# https://www.terraform.io/docs/providers/aws/r/lb_target_group.html
+resource "aws_lb_target_group" "main" {
+  name = "handson"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  port        = 80
+  target_type = "ip"
+  protocol    = "HTTP"
+
+  health_check = {
+    port = 80
+  }
+}
+
+# ALB Listener
+# https://www.terraform.io/docs/providers/aws/r/lb_listener_rule.html
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = "${aws_lb_listener.main.arn}"
+
+  priority = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.main.id}"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+}
+
+# SecurityGroup
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group" "ecs" {
+  name        = "handson-ecs"
+  description = "handson ecs"
+
+  vpc_id = "${aws_vpc.main.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-ecs"
+  }
+}
+
+# SecurityGroup Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group_rule" "ecs" {
+  security_group_id = "${aws_security_group.ecs.id}"
+
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ECS Service
+# https://www.terraform.io/docs/providers/aws/r/ecs_service.html
+resource "aws_ecs_service" "main" {
+  depends_on = ["aws_lb_listener_rule.main"]
+
+  name = "handson"
+
+  launch_type = "FARGATE"
+
+  desired_count = 1
+
+  cluster = "${aws_ecs_cluster.main.name}"
+
+  task_definition = "${aws_ecs_task_definition.main.arn}"
+
+  network_configuration = {
+    subnets         = ["${aws_subnet.private_1a.id}", "${aws_subnet.private_1c.id}", "${aws_subnet.private_1d.id}"]
+    security_groups = ["${aws_security_group.ecs.id}"]
+  }
+
+  load_balancer = [
+    {
+      target_group_arn = "${aws_lb_target_group.main.arn}"
+      container_name   = "nginx"
+      container_port   = "80"
+    },
+  ]
+}
+
+##################################################
+# HTTPS
+
+variable "domain" {
+  description = "Route 53 で管理しているドメイン名"
+  type        = "string"
+
+  #FIXME:
+  default = "poncotu.net"
+}
+
+# Route53 Hosted Zone
+# https://www.terraform.io/docs/providers/aws/d/route53_zone.html
+data "aws_route53_zone" "main" {
+  name         = "${var.domain}"
+  private_zone = false
+}
+
+# ACM
+# https://www.terraform.io/docs/providers/aws/r/acm_certificate.html
+resource "aws_acm_certificate" "main" {
+  domain_name = "${var.domain}"
+
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Route53 record
+# https://www.terraform.io/docs/providers/aws/r/route53_record.html
+resource "aws_route53_record" "validation" {
+  depends_on = ["aws_acm_certificate.main"]
+
+  zone_id = "${data.aws_route53_zone.main.id}"
+
+  ttl = 60
+
+  name    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.main.domain_validation_options.0.resource_record_type}"
+  records = ["${aws_acm_certificate.main.domain_validation_options.0.resource_record_value}"]
+}
+
+# ACM Validate
+# https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn = "${aws_acm_certificate.main.arn}"
+
+  validation_record_fqdns = ["${aws_route53_record.validation.0.fqdn}"]
+}
+
+# Route53 record
+# https://www.terraform.io/docs/providers/aws/r/route53_record.html
+resource "aws_route53_record" "main" {
+  type = "A"
+
+  name    = "${var.domain}"
+  zone_id = "${data.aws_route53_zone.main.id}"
+
+  alias = {
+    name                   = "${aws_lb.main.dns_name}"
+    zone_id                = "${aws_lb.main.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+# ALB Listener
+# https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = "${aws_lb.main.arn}"
+
+  certificate_arn = "${aws_acm_certificate.main.arn}"
+
+  port     = "443"
+  protocol = "HTTPS"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.main.id}"
+  }
+}
+
+# ALB Listener Rule
+# https://www.terraform.io/docs/providers/aws/r/lb_listener_rule.html
+resource "aws_lb_listener_rule" "http_to_https" {
+  listener_arn = "${aws_lb_listener.main.arn}"
+
+  priority = 99
+
+  action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  condition {
+    field  = "host-header"
+    values = ["${var.domain}"]
+  }
+}
+
+# Security Group Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group_rule.html
+resource "aws_security_group_rule" "alb_https" {
+  security_group_id = "${aws_security_group.alb.id}"
+
+  type = "ingress"
+
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
